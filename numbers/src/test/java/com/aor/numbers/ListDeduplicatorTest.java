af20d6c96c95d9968c5ca42a6202011e99247494
Rui Moreira <rui_scp_88@hotmail.com>
2022-10-19 10:17:28 +0100

    Numbers anottation beforeeach implemented

---
+++ numbers/src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -0,0 +1,31 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListDeduplicatorTest {
+
+    private List<Integer> list;
+    private List<Integer> expected;
+
+    @BeforeEach
+    public void helper(){
+        list = Arrays.asList(1,2,4,2,5);
+        expected = Arrays.asList(1,2,4,5);
+    }
+
+    @Test
+    public void deduplicate() {
+    //    List<Integer> list = Arrays.asList(1,2,4,2,5);
+    //    List<Integer> expected = Arrays.asList(1,2,4,5);
+
+        ListDeduplicator deduplicator = new ListDeduplicator();
+        List<Integer> distinct = deduplicator.deduplicate(list);
+
+        Assertions.assertEquals(expected, distinct);
+    }
+}
